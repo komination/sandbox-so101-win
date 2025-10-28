e48aff23ef5a27cb42326b41663b6d72ae9c2e12
komination <kominetti@proton.me>
2025-10-28 22:41:02 +0900

    feat: カメラ0と1の両方の接続チェック

--- find_camera.py
+++ find_camera.py
@@ -3,7 +3,7 @@ from lerobot.cameras.opencv.camera_opencv import OpenCVCamera
 from lerobot.cameras.configs import ColorMode, Cv2Rotation
 
 # Construct an `OpenCVCameraConfig` with your desired FPS, resolution, color mode, and rotation.
-config = OpenCVCameraConfig(
+camera_0_config = OpenCVCameraConfig(
     index_or_path=0,
     fps=30.0,
     width=640,
@@ -12,14 +12,29 @@ config = OpenCVCameraConfig(
     rotation=Cv2Rotation.NO_ROTATION
 )
 
+camera_1_config = OpenCVCameraConfig(
+    index_or_path=1,
+    fps=30.0,
+    width=640,
+    height=480,
+    color_mode=ColorMode.RGB,
+    rotation=Cv2Rotation.NO_ROTATION
+)
+
 # Instantiate and connect an `OpenCVCamera`, performing a warm-up read (default).
-camera = OpenCVCamera(config)
-camera.connect()
+camera_0 = OpenCVCamera(camera_0_config)
+camera_0.connect()
+
+camera_1 = OpenCVCamera(camera_1_config)
+camera_1.connect()
 
 # Read frames asynchronously in a loop via `async_read(timeout_ms)`
 try:
     for i in range(10):
-        frame = camera.async_read(timeout_ms=200)
-        print(f"Async frame {i} shape:", frame.shape)
+        frame0 = camera_0.async_read(timeout_ms=200)
+        frame1 = camera_1.async_read(timeout_ms=200)
+        print(f"Async frame {i} from camera 0 shape:", frame0.shape)
+        print(f"Async frame {i} from camera 1 shape:", frame1.shape)
 finally:
-    camera.disconnect()
+    camera_0.disconnect()
+    camera_1.disconnect()
